8a5386d79f10c17dc9451d29f0ef15347719cb36
Alexandre Gros <xelaflash@gmail.com>
2020-06-23 11:25:21 -0400

    add active effect for link

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -7,3 +7,17 @@ window.addEventListener('keydown', handleFirstTab);
 // sidenav
 const sidenavTrigger = document.getElementById('sidenav-trigger');
 sidenavTrigger.addEventListener('click', toogleNav);
+
+const current = document.location.pathname.split('/')[1];
+const navs = document.querySelectorAll('.navbar-link');
+
+console.log(current);
+console.log(navs);
+
+Array.from(navs).forEach(el => {
+  const truncatedHref = el.href.split('/')[3];
+  console.log(truncatedHref);
+  if (truncatedHref === current) {
+    el.classList.add('active');
+  }
+});
