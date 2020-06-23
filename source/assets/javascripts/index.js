d9d6c23a848ae33702dd20d0574c264ef652716b
Alexandre Gros <xelaflash@gmail.com>
2020-06-23 10:33:22 -0400

    add outline focus for tabbing only + reponsive navbar + pre-navbar

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,4 +1,9 @@
 import { toogleNav } from './components/sidenav';
+import { handleFirstTab } from './components/keyboard_focus';
 
+// outline for keyboard user on focus
+window.addEventListener('keydown', handleFirstTab);
+
+// sidenav
 const sidenavTrigger = document.getElementById('sidenav-trigger');
 sidenavTrigger.addEventListener('click', toogleNav);
