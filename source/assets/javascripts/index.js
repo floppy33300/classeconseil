2b163776a359b3d729d29b5ca490cff0ab9b14c6
Alexandre Gros <xelaflash@gmail.com>
2020-06-23 11:30:44 -0400

    js tabs code in sperate file

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,5 +1,6 @@
 import { toogleNav } from './components/sidenav';
 import { handleFirstTab } from './components/keyboard_focus';
+import './components/tabs';
 
 // outline for keyboard user on focus
 window.addEventListener('keydown', handleFirstTab);
@@ -7,17 +8,3 @@ window.addEventListener('keydown', handleFirstTab);
 // sidenav
 const sidenavTrigger = document.getElementById('sidenav-trigger');
 sidenavTrigger.addEventListener('click', toogleNav);
-
-const current = document.location.pathname.split('/')[1];
-const navs = document.querySelectorAll('.navbar-link');
-
-console.log(current);
-console.log(navs);
-
-Array.from(navs).forEach(el => {
-  const truncatedHref = el.href.split('/')[3];
-  console.log(truncatedHref);
-  if (truncatedHref === current) {
-    el.classList.add('active');
-  }
-});
