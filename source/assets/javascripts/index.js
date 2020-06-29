b7b082341bd3d1b687f97f52f6b62280ac3ca569
Alexandre Gros <xelaflash@gmail.com>
2020-06-29 13:11:21 -0400

    add carousel for clients logo

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,3 +1,5 @@
+import Glide, { Autoplay, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm';
+
 import { toogleNav } from './components/sidenav';
 import { handleFirstTab } from './components/keyboard_focus';
 import './components/tabs';
@@ -8,3 +10,27 @@ window.addEventListener('keydown', handleFirstTab);
 // sidenav
 const sidenavTrigger = document.getElementById('sidenav-trigger');
 sidenavTrigger.addEventListener('click', toogleNav);
+
+// mounting Carousel
+// new Glide('.glide').mount({ Autoplay });
+new Glide('.glide', {
+  type: 'carousel',
+  autoplay: 4000,
+  focusAt: 'center',
+  gap: 0,
+  animationDuration: 4000,
+  animationTimingFunc: 'linear',
+  peek: 50,
+  perView: 4,
+  breakpoints: {
+    1100: {
+      perView: 3,
+    },
+    800: {
+      perView: 2,
+    },
+    600: {
+      perView: 1,
+    },
+  },
+}).mount({ Autoplay, Breakpoints });
