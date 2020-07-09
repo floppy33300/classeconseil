c8e468de0b2a69a760680228c72612133947ec74
Alexandre Gros <xelaflash@gmail.com>
2020-07-09 11:20:17 -0400

    add favicon

--- config.rb
+++ config.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Load Sass from node_modules
 config[:sass_assets_paths] << File.join(root, 'node_modules')
 
@@ -7,14 +9,14 @@ set :images_dir, 'assets/images'
 set :js_dir,     'assets/javascripts'
 
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 2 versions'
 end
 
 activate :external_pipeline,
-  name: :webpack,
-  command: build? ? 'yarn run build' : 'yarn run start',
-  source: 'dist',
-  latency: 1
+         name: :webpack,
+         command: build? ? 'yarn run build' : 'yarn run start',
+         source: 'dist',
+         latency: 1
 
 # ------ Uncomment and adapt if you want to use i18n ------
 # activate :i18n, mount_at_root: :fr, langs: [:fr, :en]
@@ -31,12 +33,42 @@ end
 
 configure :build do
   activate :minify_css
-  # ------ We commented this command as it prevented us to deploy our Middleman project on GitHub Pages ------
-  # activate :minify_javascript
+  activate :minify_javascript
   activate :minify_html, remove_input_attributes: false
   activate :asset_hash
   activate :relative_assets
   set :relative_links, true
+  activate :favicon_maker do |f|
+    f.template_dir  = File.join(root, 'source')
+    f.output_dir    = File.join(root, 'build')
+    f.icons = {
+      '_favicon_template.png' => [
+        { icon: 'apple-touch-icon-180x180-precomposed.png' },
+        { icon: 'apple-touch-icon-152x152-precomposed.png' },
+        { icon: 'apple-touch-icon-144x144-precomposed.png' },
+        { icon: 'apple-touch-icon-120x120-precomposed.png' },
+        { icon: 'apple-touch-icon-114x114-precomposed.png' },
+        { icon: 'apple-touch-icon-76x76-precomposed.png' },
+        { icon: 'apple-touch-icon-72x72-precomposed.png' },
+        { icon: 'apple-touch-icon-60x60-precomposed.png' },
+        { icon: 'apple-touch-icon-57x57-precomposed.png' },
+        { icon: 'apple-touch-icon-precomposed.png', size: '57x57' },
+        { icon: 'apple-touch-icon.png', size: '57x57' },
+        { icon: 'favicon-196x196.png' },
+        { icon: 'favicon-160x160.png' },
+        { icon: 'favicon-96x96.png' },
+        { icon: 'favicon-32x32.png' },
+        { icon: 'favicon-16x16.png' },
+        { icon: 'favicon.png', size: '16x16' },
+        { icon: 'favicon.ico', size: '64x64,32x32,24x24,16x16' },
+        { icon: 'mstile-70x70.png', size: '70x70' },
+        { icon: 'mstile-144x144.png', size: '144x144' },
+        { icon: 'mstile-150x150.png', size: '150x150' },
+        { icon: 'mstile-310x310.png', size: '310x310' },
+        { icon: 'mstile-310x150.png', size: '310x150' }
+      ]
+    }
+  end
 end
 
 activate :deploy do |deploy|
